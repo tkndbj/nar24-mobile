7a8698b5523f6fd053cd420afcb29071dc579998
TekinDabaj <tekin_dabaj@hotmail.com>
2026-04-06 14:11:27 +0300

    office pc update

--- lib/services/typesense_service.dart
+++ lib/services/typesense_service.dart
@@ -412,7 +412,7 @@ class TypeSenseService {
     String? queryBy,
     String? includeFields,
     String? facetBy,
-    int maxFacetValues = 100,
+    int maxFacetValues = 200,
   }) async {
     final uri = _searchUri(indexName);
 
@@ -918,7 +918,7 @@ class TypeSenseService {
     String? queryBy,
     String? includeFields,
     String facetBy = _specFacetFields,
-    int maxFacetValues = 100,
+    int maxFacetValues = 200,
     Map<String, List<String>> disjunctiveFilters = const {},
     List<String> numericFilters = const [],
   }) async {
